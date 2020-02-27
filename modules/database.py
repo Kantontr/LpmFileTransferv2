36a19c8d5c2f7fe83170f172a788c229d1341514
Al Capone <kantontr@gmail.com>
2020-02-27 22:12:22 +0100

    first commit

--- modules/database.py
+++ modules/database.py
@@ -1,22 +1,21 @@
 import os
 
 
-#d = {"Baltimore Ravens": [13, 3]}
-#d["Baltimore Ravens"][0] += 1
-#print d
+# d = {"Baltimore Ravens": [13, 3]}
+# d["Baltimore Ravens"][0] += 1
+# print d
 # {"Baltimore Ravens": [14, 3]}
 
 class database:
+    saved_user = {}  # saved user name, ip, port
 
-    saved_user = {} #saved user name, ip, port
-
-    def checkIfUserExist(self,username):
+    def checkIfUserExist(self, username):
         if username in self.saved_user:
             print("Username exist")
             return True
         return False
 
-    def checkIfIpExist(self,ip):
+    def checkIfIpExist(self, ip):
         for i, j in self.saved_user.items():
             if self.saved_user[i][0] == ip:
                 print("IP exist")
@@ -24,10 +23,10 @@ class database:
         return False
 
     def printDatabase(self):
-        for i,j in self.saved_user.items():
-            print(i,j)
+        for i, j in self.saved_user.items():
+            print(i, j)
 
-    def AddUser(self,new_username,new_ip,new_port,new_comment):
+    def AddUser(self, new_username, new_ip, new_port, new_comment):
 
         if len(new_username) < 0:
             return "Username is empty"
@@ -37,28 +36,30 @@ class database:
             return "port is empty"
         if not self.checkIfUserExist(new_username):
             if not self.checkIfIpExist(new_ip):
-                self.saved_user[new_username] = [new_ip, new_port,new_comment]
+                self.saved_user[new_username] = [new_ip, new_port, new_comment]
                 if self.checkIfUserExist(new_username):
                     self.SaveDatabaseToFile()
                     return "Operation Succesfull"
         return "Operation failed"
 
-    def EditUser(self,old_username,new_username,new_ip,new_port,new_comment):
+    def EditUser(self, old_username, new_username, new_ip, new_port, new_comment):
 
         if not self.checkIfUserExist(old_username):
             return "Error finding existing user"
-        backup = [old_username, self.saved_user[old_username][0], self.saved_user[old_username][1], self.saved_user[old_username][2]] #creates backup in case new data is incorrect
+
+        backup = [old_username, self.saved_user[old_username][0], self.saved_user[old_username][1],
+                  self.saved_user[old_username][2]]  # creates backup in case new data is incorrect
         del self.saved_user[old_username]
 
-        result = self.AddUser(new_username,new_ip,new_port,new_comment)
+        result = self.AddUser(new_username, new_ip, new_port, new_comment)
+
         if result == "Operation Succesfull":
             self.SaveDatabaseToFile()
             return "Operation Successfull"
-
         else:
             result = self.AddUser(backup[0], backup[1], backup[2], backup[3])
-            if result != "Operation Succesfull":
-                print ("Fatal error editing entry! Reloading backup from file")
+            if result != "Operation Succesfull": #if backup cannot be loaded, load a backup from file
+                print("Fatal error editing entry! Reloading backup from file")
                 self.LoadDatabaseFromFile()
                 return "Operation Failed. No changes"
 
